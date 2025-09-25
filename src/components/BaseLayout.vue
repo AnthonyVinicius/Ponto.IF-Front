8cd7a2a48a3f7634dd382f8e1513921837096b23
Anthony <anthonyvinicius1312@gmail.com>
2025-09-25 15:58:24 -0300

    Instalação do Vue Router e criação do BaseLayout

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="page-container">
+        <main class="content">
+          <h1>teste</h1>
+            <slot></slot>
+        </main>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
