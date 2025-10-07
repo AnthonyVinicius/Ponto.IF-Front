4eec1b69163729838f32eebf7b954fdd58c5efda
Anthony <anthonyvinicius1312@gmail.com>
2025-10-06 21:12:47 -0300

    testando CI

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { ref } from 'vue'
 import Footer from './Footer.vue'
-import NavBar from './NavBar.vue'
+import NavBar from './Navbar.vue'
 import Sidebar from './Sidebar.vue'
 
 const isExpanded = ref(true)
-
+const on = false
 const toggleSidebar = () => {
   isExpanded.value = !isExpanded.value
 }
@@ -13,24 +13,20 @@ const toggleSidebar = () => {
 
 <template>
   <div class="flex flex-col min-h-screen">
-    <!-- Barra de navegação no topo -->
-    <NavBar />
 
-    <!-- Área central: Sidebar + Conteúdo -->
+    <NavBar />
     <div class="flex flex-1">
-      <!-- Sidebar ocupa toda altura entre NavBar e Footer -->
-      <Sidebar 
+
+      <Sidebar v-if="on"
         :is-expanded="isExpanded" 
         @toggle-sidebar="toggleSidebar"
       />
 
-      <!-- Conteúdo da página, ao lado da Sidebar -->
       <main class="flex-1 p-6 bg-gray-50 overflow-y-auto transition-all duration-300">
         <slot></slot>
       </main>
     </div>
-
-    <!-- Rodapé sempre no final -->
-    <Footer />
+ 
+    <Footer></Footer>
   </div>
 </template>
