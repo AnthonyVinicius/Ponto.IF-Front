718263c85ab122e6b44396060caf06c2d1974db8
Anthony <anthonyvinicius1312@gmail.com>
2025-11-02 22:30:14 -0300

    Botão conectado com BiometricAPI

--- src/api/ApiRegistry.js
+++ src/api/ApiRegistry.js
@@ -1,7 +1,9 @@
 import { registerIfApi } from "./registerIfApi";
 import { pontoIfApi } from "./pontoIfApi";
+import { biometricApi } from "./biometricApi";
 
 export const ApiRegistry = {
   pontoIF: pontoIfApi,
   registerIF: registerIfApi,
+  biometric: biometricApi
 };
--- src/services/BiometricDAO.js
+++ src/services/BiometricDAO.js
@@ -1,9 +1,14 @@
 import GenericDAO from "./GenericDAO";
-import { ApiRegistry } from "../api/ApiRegistry"; 
+import { ApiRegistry } from "../api/ApiRegistry";
 
 class BiometricDAO extends GenericDAO {
   constructor() {
-    super(ApiRegistry.registerIF, "/biometric");
+    super(ApiRegistry.biometric, "/biometric");
+  }
+
+    async insertSample(data) {
+    const res = await this.api.post(`${this.baseURL}/sample`, data);
+    return res.data;
   }
 }
 
